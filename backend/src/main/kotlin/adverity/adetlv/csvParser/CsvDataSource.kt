aa99a8c09a9857028a62625f7f3f241a91d73e48
rbidas <rb9@o2.pl>
2019-10-02 03:06:36 +0200

    update reading  csv file

--- backend/src/main/kotlin/adverity/adetlv/csvParser/CsvDataSource.kt
+++ backend/src/main/kotlin/adverity/adetlv/csvParser/CsvDataSource.kt
@@ -1,27 +1,22 @@
 package adverity.adetlv.csvParser
 
 import io.micronaut.context.annotation.Value
-import org.apache.commons.csv.CSVFormat
-import org.apache.commons.csv.CSVParser
 import java.nio.file.Files
 import java.nio.file.Paths
 import javax.inject.Singleton
-
-const val DATE: String = "Date"
-const val DATASOURCE: String = "Datasource"
-const val CAMPAIGN: String = "Campaign"
-const val CLICKS: String = "Clicks"
-const val IMPRESSIONS: String = "Impressions"
 
 @Singleton
 class CsvDataSource(@Value("\${datasource.path}") val path: String) : DataSource {
 
     override fun data(): Collection<DataSourceDto> {
         val reader = Files.newBufferedReader(Paths.get(path))
-        val csvParser = CSVParser(reader, CSVFormat.EXCEL.withSkipHeaderRecord().withHeader(DATE, DATASOURCE, CAMPAIGN, CLICKS, IMPRESSIONS))
-
         val toReturn = mutableListOf<DataSourceDto>()
-        csvParser.mapTo(toReturn, { DataSourceDto(it.get(DATE), it.get(DATASOURCE), it.get(CAMPAIGN), convert(it.get(CLICKS)), convert(it.get(IMPRESSIONS))) })
+        //read line by line to avoid read very big files into memory
+        val header = reader.readLine()
+        reader.forEachLine { line ->
+            val split = line.split(",")
+            toReturn.add(DataSourceDto(split[0], split[1], split[2], convert(split[3]), convert(split[4])))
+         }
 
         return toReturn
     }
